f99533789af54b58a18402ba9c71de476695ce56
Rikikudo <trunghieu99tt@gmail.com>
2020-10-19 01:37:59 +0700

    Updates

---
+++ precache-manifest.1e43d70623484ca9f922bf0403c51476.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2e351cc93045634375bfe712b31607",
+    "url": "/hanoitravel/index.html"
+  },
+  {
+    "revision": "593f7a43436caf0215ca",
+    "url": "/hanoitravel/static/css/2.4e0b54ea.chunk.css"
+  },
+  {
+    "revision": "9217d2efde67bdbffe30",
+    "url": "/hanoitravel/static/css/main.5a912c1b.chunk.css"
+  },
+  {
+    "revision": "593f7a43436caf0215ca",
+    "url": "/hanoitravel/static/js/2.dc686745.chunk.js"
+  },
+  {
+    "revision": "c4a943e7ed0bc88a2d72cdf46f045905",
+    "url": "/hanoitravel/static/js/2.dc686745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9217d2efde67bdbffe30",
+    "url": "/hanoitravel/static/js/main.22d2572c.chunk.js"
+  },
+  {
+    "revision": "c6202093deb6f3604af7",
+    "url": "/hanoitravel/static/js/runtime-main.78124930.js"
+  },
+  {
+    "revision": "37b5c5d914d1008fae09f6d6f20c953b",
+    "url": "/hanoitravel/static/media/bg-01.37b5c5d9.jpg"
+  }
+]);
